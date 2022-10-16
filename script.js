87a4c523cdb904a05c958a1b00894c0d6b64372a
eschroer <schroer.emerson@gmail.com>
2022-10-15 23:32:43 -0500

    Add changeGridSize function

--- script.js
+++ script.js
@@ -5,9 +5,13 @@ let currentColor = DEFAULT_COLOR
 let currentSize = DEFAULT_SIZE
 
 const container = document.querySelector(".container");
-const resetButton = document.querySelector("button");
+const resetButton = document.querySelector(".reset");
+const colorButton = document.querySelector(".color")
+const gridSizeSlider = document.querySelector(".size")
 
 resetButton.addEventListener("click", clearGrid);
+colorButton.addEventListener("click", changeColor);
+gridSizeSlider.addEventListener("change", changeGridSize)
 
 function createGrid(size) {
     for (let i = 0; i<(size * size); i++) {
@@ -15,6 +19,8 @@ function createGrid(size) {
         pixel.classList.add("pixel");
         container.appendChild(pixel);
         pixel.addEventListener("mouseover", changeColor)
+        container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+        container.style.gridTemplateRows = `repeat(${size}, 1fr)`
     } 
 }
 function changeColor(event){
@@ -27,4 +33,10 @@ function clearGrid(){
         item.style.backgroundColor = `${"rgb(219, 223, 223, 10)"}`
     })
 }
+function changeGridSize(event){
+    currentSize = parseInt(event.target.value)
+    clearGrid()
+    createGrid(currentSize)
+}
+
 createGrid(currentSize);
